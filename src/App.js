08009a2fe9b1d90b7361d307a3ebd8129b375261
israel larrondo <arsistyle@gmail.com>
2020-02-23 13:53:37 -0300

    Botón WhatsApp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /** DATA **/
-import { Global } from './data'
+import { Global } from './data';
 
 /** COMPONETNS **/
 import Header from './components/Header';
@@ -10,12 +10,12 @@ import Header from './components/Header';
 // images
 import Logo from './assets/img/logo.svg';
 // Styles
-import './assets/scss/ars1/ars1.scss'
+import './assets/scss/ars1/ars1.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header logo={Logo} alt={Global.title}/>
+      <Header logo={Logo} alt={Global.title} wspurl={Global.wsp.url} wspicon={Global.wsp.icon} wsptext={Global.wsp.text}/>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,11 +5,17 @@ import '../../assets/scss/style/components/Header.scss';
 const Header = props => {
   return (
     <header className="header">
-      {
-        props.logo ? (<img src={props.logo} alt={props.alt} />): ''
-      }
+      {props.logo ? <img src={props.logo} alt={props.alt} /> : ''}
+      {props.wspurl ? (
+        <a href={props.wspurl} target="_blank" rel="noopener noreferrer">
+          <props.wspicon />
+          <span>{props.wsptext}</span>
+        </a>
+      ) : (
+        ''
+      )}
     </header>
-  )
-}
+  );
+};
 
 export default Header;
