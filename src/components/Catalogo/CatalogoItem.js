27d874baa1f3a6856b4fb4f9143ab7bf286393b7
israel larrondo <arsistyle@gmail.com>
2020-03-21 17:54:48 -0300

    Cambio en texto de extras

--- src/components/Catalogo/CatalogoItem.js
+++ src/components/Catalogo/CatalogoItem.js
@@ -9,7 +9,7 @@ const CatalogoItem = props => {
     setImage(true);
   };
 
-  let extraData = props.extraData ? `(_Extra: ${props.extraData}_)` : '';
+  let extraData = props.extraData ? `(${props.extraData})` : '';
 
   return (
     <div className='col-xs-12 col-sm-6 col-md-4 col-lg-3'>
